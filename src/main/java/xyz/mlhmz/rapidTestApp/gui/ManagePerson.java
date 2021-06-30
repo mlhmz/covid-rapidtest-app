90604465982436597eff0be8588b7341a7c5ec0f
mlhmz <malek.hamza@gmx.de>
2021-06-30 20:11:37 +0200

    Changed in the Manage Person Menu from List to Table, changed pom.xml to v1.1

--- src/main/java/xyz/mlhmz/rapidTestApp/gui/ManagePerson.java
+++ src/main/java/xyz/mlhmz/rapidTestApp/gui/ManagePerson.java
@@ -7,33 +7,36 @@ import xyz.mlhmz.rapidTestApp.database.entities.Person;
 import xyz.mlhmz.rapidTestApp.database.entities.Test;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class ManagePerson {
     public JFrame frame;
-    private JButton deleteBtn;
 
-    // TODO: Replace List with Table
-    // Will be added in Version 1.1
-    private JList personList;
-    private DefaultListModel personListModel;
-
+    private JTable personTable;
 
     private JPanel panel;
-    private JLabel deleteLabel;
-    private JButton createBtn;
-    private JButton editBtn;
+    private JLabel manageLabel;
+    private JButton createBtn, editBtn, deleteBtn;
+
 
     public ManagePerson() {
+        // Set Names
+        createBtn.setName("Create");
+        editBtn.setName("Edit");
+        deleteBtn.setName("Delete");
+        manageLabel.setName("Manage the Persons");
+
         deleteBtn.addActionListener((event) -> {
-            if (personList.isSelectionEmpty()) {
+
+            if (personTable.getSelectionModel().isSelectionEmpty()) {
                 JOptionPane.showMessageDialog(null, "Please choose the Person you want to delete.");
                 return;
             }
 
             Repository repository = new Tests();
-            Person person = (Person) personList.getSelectedValue();
+            Person person = (Person) personTable.getValueAt(personTable.getSelectedRow(), 0);
 
             for (Object o : repository.get()) {
                 Test test = (Test) o;
@@ -45,23 +48,24 @@ public class ManagePerson {
             repository = new Persons();
 
             repository.delete(person.getId());
-            reloadList();
+            reloadTable();
         });
 
         editBtn.addActionListener(e -> {
-            if (personList.isSelectionEmpty()) {
+            if (personTable.getSelectionModel().isSelectionEmpty()) {
                 JOptionPane.showMessageDialog(null, "Please choose the Person you want to edit.");
                 return;
             }
-
-            Person person = (Person) personList.getSelectedValue();
+            Repository repository = new Persons();
+            Long personId = Long.parseLong(personTable.getValueAt(personTable.getSelectedRow(), 0).toString());
+            Person person = (Person) repository.getById(personId);
 
             EditPerson editPerson = new EditPerson();
             editPerson.run(person);
             editPerson.frame.addWindowListener(new WindowAdapter() {
                 @Override
                 public void windowClosed(WindowEvent e) {
-                    reloadList();
+                    reloadTable();
                 }
             });
         });
@@ -72,12 +76,10 @@ public class ManagePerson {
             createPerson.frame.addWindowListener(new WindowAdapter() {
                 @Override
                 public void windowClosed(WindowEvent e) {
-                    reloadList();
+                    reloadTable();
                 }
             });
         });
-
-        personList.setLayoutOrientation(JList.VERTICAL);
 
 
     }
@@ -85,7 +87,7 @@ public class ManagePerson {
 
 
     public void run() {
-        reloadList();
+        reloadTable();
         frame = new JFrame("Manage Person");
         frame.add(panel);
         frame.pack();
@@ -93,11 +95,43 @@ public class ManagePerson {
 
     }
 
-    public void reloadList() {
-        Repository repository = new Persons();
-        personListModel = new DefaultListModel<Person>();
-        personListModel.addAll(repository.get());
+    public void reloadTable() {
+        Repository repository;
 
-        personList.setModel(personListModel);
+        // JTable Content
+        DefaultTableModel newTableModel = new DefaultTableModel() {
+            public boolean isCellEditable(int rowIndex, int mColIndex) {
+                return false;
+            }
+        };
+
+        String[] tableColumns = {
+                "Person ID",
+                "First Name",
+                "Last Name",
+                "Address",
+                "Phone Number"
+        };
+
+
+        for (String column : tableColumns) {
+            newTableModel.addColumn(column);
+        }
+
+        repository = new Persons();
+
+        for (Object o : repository.get()) {
+            Person person = (Person) o;
+            Object[] data = {person.getId(),
+            person.getFirstName(),
+            person.getLastName(),
+            person.getAddress(),
+            person.getPhoneNumber()};
+            newTableModel.addRow(data);
+
+
+        }
+
+        personTable.setModel(newTableModel);
     }
 }
